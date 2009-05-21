3abffe17a9812a53988f605b2e751937f4876620
eckes <eckes@4814f1d8-a8f5-11de-ac14-117a7284e870>
2009-05-21 10:00:26 +0000

    work going on
    git-svn-id: https://eckes.googlecode.com/svn/trunk/wandern@11 4814f1d8-a8f5-11de-ac14-117a7284e870

--- index.php
+++ index.php
@@ -13,44 +13,48 @@
         var g_LAT_MAX       = 0;
         var g_LON_MIN       = 0;
         var g_LON_MAX       = 0;
-        var g_MAPCENTER     = 0;
         var g_INITIALIZED   = 0;
+        var g_MARKERLIST    = new Array();
         var g_MAPBOUNDS;
         var g_MANAGER;
         var g_MAP;
+        var g_ICON;
 
-        function updateCenter(a_long, a_lat)
+        function MarkEntry(a_tag, a_marker)
         {
-            g_LAT_MAX = Math.max(a_lat, g_LAT_MAX);
-            if(0 != g_LAT_MIN)
-            {
-                g_LAT_MIN = Math.min(a_lat, g_LAT_MIN);
-            }
-            else
-            {
-                g_LAT_MIN = g_LAT_MAX;
-            }
-            g_LON_MAX = Math.max(a_long, g_LON_MAX);
-            if(0 != g_LON_MIN)
-            {
-                g_LON_MIN = Math.min(a_long, g_LON_MIN);
-            }
-            else
-            {
-                g_LON_MIN = g_LON_MAX;
-            }
-            var lat = (g_LAT_MIN + g_LAT_MAX)/2;
-            var lon = (g_LON_MIN + g_LON_MAX)/2;
-            g_MAPCENTER = new google.maps.LatLng(lat,lon);
+            this.m_tag      = a_tag;
+            this.m_marker   = a_marker;
         }
 
-        function showMark(a_long, a_lat, a_text)
+        function addMark(a_long, a_lat, a_text, a_tag)
         {
             var pos     = new google.maps.LatLng(a_lat, a_long);
-            var options = { title: a_text };
+            var options = {title: a_text, bouncy: true, icon:g_ICON};
+            var mark    = new google.maps.Marker(pos, options);
+
+            /* hide the mark and add it to our markerlist */
+            var me = new MarkEntry(a_tag, mark);
+            g_MARKERLIST.push(me);
+            me = null;
+
+            g_MAPBOUNDS.extend(pos);
+            g_MANAGER.addMarker(mark, 10);
+            var l_more = "<a href=\"javascript:doHide(\'" + a_tag +"\')\">hide</a>";
+            GEvent.addListener(mark, "click", function(){mark.openInfoWindowHtml(a_text + "<br>" + l_more);});
+        }
+
+        function showHome(a_long, a_lat, a_text)
+        {
+            var homeIcon = new google.maps.Icon();
+            homeIcon.image              = "images/home.png";
+            homeIcon.iconSize           = new google.maps.Size(22,22);
+            homeIcon.iconAnchor         = new google.maps.Point(0,25);
+            homeIcon.infoWindowAnchor   = new google.maps.Point(11,0);
+
+            var pos     = new google.maps.LatLng(a_lat, a_long);
+            var options = {title: a_text, icon:homeIcon};
             var mark    = new google.maps.Marker(pos, options);
             g_MAPBOUNDS.extend(pos);
-            updateCenter(a_long, a_lat);
             g_MANAGER.addMarker(mark, 10);
             g_MANAGER.refresh();
             GEvent.addListener(mark, "click", function(){mark.openInfoWindowHtml(a_text);});
@@ -58,12 +62,54 @@
 
         function initialize() {
             if(g_INITIALIZED) return;
+            g_INITIALIZED = 1;
+
             g_MAP = new google.maps.Map2(document.getElementById("map"));
             g_MANAGER = new google.maps.MarkerManager(g_MAP);
             g_MAPBOUNDS = new google.maps.LatLngBounds();
-            showMark(<?=$g_homelon?>,<?=$g_homelat?>, "Daheim");
-            g_MAP.setCenter(g_MAPCENTER, g_MAP.getBoundsZoomLevel(g_MAPBOUNDS));
-            g_INITIALIZED = 1;
+
+            g_ICON = new google.maps.Icon();
+            g_ICON.image                = "images/wanderparkplatz.png"; 
+            g_ICON.iconSize             = new google.maps.Size(21.5,32);
+            g_ICON.iconAnchor           = new google.maps.Point(0,36);
+            g_ICON.infoWindowAnchor     = new google.maps.Point(5,2);
+        }
+
+        function doHide(a_name)
+        {
+            document.getElementById(a_name).checked = false;
+            return cbChanged(a_name);
+        }
+
+        function cbChanged(a_name)
+        {
+            var bChecked = document.getElementById(a_name).checked;
+            var me      = null;
+            var i       = 0;
+            for(i = 0; i < g_MARKERLIST.length; i++)
+            {
+                me = g_MARKERLIST[i];
+                if(null == me)
+                {
+                    continue; /* skip the gap */
+                }
+                if(a_name == me.m_tag)
+                {
+                    break;
+                }
+            }
+            if(g_MARKERLIST.length <= i)
+            {
+                alert("not found");
+            }
+            if(false == bChecked)
+            {
+                me.m_marker.hide();
+            }
+            else
+            {
+                me.m_marker.show();
+            }
         }
 
         //google.setOnLoadCallback(initialize);
@@ -71,6 +117,7 @@
 
         var TSort_Data = new Array ('walks', 's', 's', 'f', 'f');
         var TSort_Classes = new Array ('table_odd', 'table_even');
+        var TSort_Initial = 0;
         tsRegister();
 
         </script>
@@ -88,10 +135,10 @@
             <thead>
                 <tr><th>Tag</th><th>Name</th><th>Laenge</th><th>Dauer</th></tr>
             </thead>
-        <div id="map" style="width: 400px; height: 400px"></div>
+        <div id="map" style="width: 800px; height: 400px"></div>
 
 <?php
-    $sql = "SELECT * FROM `walks` LIMIT 0, 10 ";
+    $sql = "SELECT * FROM `walks` LIMIT 0, 50 ";
     $sql_host = "localhost";
     $sql_user = "root";
     $sql_pass = "";
@@ -102,20 +149,27 @@
     $res = mysql_query($sql, $db);
     $i = 0;
 
+    /* TODO:
+     * - take the results of the query and create the neccessary points:
+     *      o create a marker for each
+     *      o take these markers into our list
+     *      o hide the markers until their checkbox is selected
+     * */
     while($row=mysql_fetch_array($res))
     {
-        echo "<tr><td>$row[Tag] $row[Lon] $row[Lat]</td><td>$row[Name]</td><td>$row[Laenge]</td><td>$row[Dauer]</td></tr>\r\n";
+        echo "<tr><td><input type=\"checkbox\" checked name=\"tag\" id=\"$row[Tag]\" value=\"$row[Tag]\" onchange=\"cbChanged('$row[Tag]')\"> $row[Tag]</td><td>$row[Name]</td><td>$row[Laenge]</td><td>$row[Dauer]</td></tr>\r\n";
     }
     echo "</table>";
 
     echo "<script type=\"text/javascript\">\n";
-    echo "initialize();";
+    echo "initialize();\n";
+    echo "showHome($g_homelon, $g_homelat, 'Daheim');\n";
     $res = mysql_query($sql, $db);
     while($row=mysql_fetch_array($res))
     {
-        echo "showMark($row[Lat], $row[Lon], '$row[Name]');\n";
+        echo "addMark($row[Lat], $row[Lon], '$row[Name]', '$row[Tag]');\n";
     }
-    echo "g_MAP.setCenter(g_MAPCENTER, g_MAP.getBoundsZoomLevel(g_MAPBOUNDS));\n";
+    echo "g_MAP.setCenter(g_MAPBOUNDS.getCenter(), g_MAP.getBoundsZoomLevel(g_MAPBOUNDS));\n";
     echo "</script>";
 
     mysql_close($db);
