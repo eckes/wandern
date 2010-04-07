86730a56d0e48f5d300820bad39238f473b2b463
MeliundEckes <MeliundEckes@4814f1d8-a8f5-11de-ac14-117a7284e870>
2010-04-07 06:52:57 +0000

    git-svn-id: https://eckes.googlecode.com/svn/trunk/wandern@100 4814f1d8-a8f5-11de-ac14-117a7284e870

--- track.php
+++ track.php
@@ -13,7 +13,7 @@ function get_track($a_id)
 {
   $path   = "kml/" . $a_id . ".kml";
   $result = null;
-  if(file_exists($path))
+  if(has_track($a_id))
   {
     $xmlstr = file_get_contents($path);
     $xmlstr = str_replace('xmlns=', 'ns=', $xmlstr);
@@ -25,17 +25,25 @@ function get_track($a_id)
       $retarr = array();
       $tmparr = preg_split('/[\s]+/', $result, 0, PREG_SPLIT_NO_EMPTY);
       $latlng = array();
+      $retstr = '"new google.maps.Polyline([';
       for($i = 0; $i < count($tmparr); $i+=3)
       {
+        if($i)
+        {
+          $retstr = $retstr . ', ';
+        }
         $coord = preg_split('/,/', $tmparr[$i], 0, PREG_SPLIT_NO_EMPTY);
         $latlng[lng] = $coord[0];
         $latlng[lat] = $coord[1];
         array_push($retarr, $latlng);
+        $retstr = $retstr . 'new google.maps.LatLng(' . $coord[1] . ', ' . $coord[0] . ")";
       }
+      $retstr = $retstr . "]);\"\n";
     }
   }
-  return $retarr;
+  return $retstr;
   /*
+   *'new google.maps.Polyline([ new google.maps.LatLng(49.4962466666667, 10.80186), new google.maps.LatLng(49.4964216666667, 10.8028566666667) ], "#ff0000", 5, 1);'
     GPolyLine([
       {lat:LAT, long:LONG},
       {lat:LAT, long:LONG},
@@ -44,6 +52,13 @@ function get_track($a_id)
       {lat:LAT, long:LONG}
       ]);
    */
+}
+
+function has_track($a_id)
+{
+  $path   = "kml/" . $a_id . ".kml";
+  $result = null;
+  return(file_exists($path));
 }
 
 ?>
